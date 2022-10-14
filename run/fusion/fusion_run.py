6cd2e7ad3ef1a9e2ab196679f8034ed010a38c05
Ryan Kortvelesy <rk627@cam.ac.uk>
2022-10-14 22:53:35 +0100

    removed old fusion

--- run/fusion/fusion_run.py
+++ run/fusion/fusion_run.py
@@ -1,7 +1,6 @@
-import torch
 import wandb
 from torch.optim import Adam
-from run.fusion.nested.fusion_dataset import ObsEnv
+from run.fusion.fusion_dataset import ObsEnv
 from fusion_model import FusionModel
 from visualiser import Visualiser
 from sae.util import *
